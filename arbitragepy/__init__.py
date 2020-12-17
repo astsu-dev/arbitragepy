9fe095744ccf68f77fe53c412ba89b42783ff65e
astsu <you@example.com>
2020-12-17 20:24:12 +0200

    Added is_compatible_quantity_increments function.

--- arbitragepy/__init__.py
+++ arbitragepy/__init__.py
@@ -15,3 +15,17 @@ def get_spread(ask_price: Decimal, bid_price: Decimal) -> Decimal:
     """
 
     return (bid_price / ask_price - 1) * 100
+
+
+def is_compatible_quantity_increments(qty_inc1: Decimal, qty_inc2: Decimal) -> bool:
+    """Returns True if `qty_inc1` % `qty_inc2` == 0 or `qty_inc2` % `qty_inc1` == 0
+
+    Args:
+        qty_inc1 (Decimal)
+        qty_inc2 (Decimal)
+
+    Returns:
+        bool
+    """
+
+    return qty_inc1 % qty_inc2 == 0 or qty_inc2 % qty_inc1 == 0
--- tests/test_arbitragepy.py
+++ tests/test_arbitragepy.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-from arbitragepy import get_spread
+from arbitragepy import get_spread, is_compatible_quantity_increments
 
 
 def test_get_spread() -> None:
@@ -8,3 +8,25 @@ def test_get_spread() -> None:
     assert get_spread(Decimal("200"), Decimal("100")) == Decimal("-50")
     assert get_spread(Decimal("0.5"), Decimal("2.0")) == Decimal("300")
     assert get_spread(Decimal("0.5"), Decimal("0.75")) == Decimal("50")
+
+
+def test_is_compatible_quantity_increments() -> None:
+    assert is_compatible_quantity_increments(Decimal("4"), Decimal("2"))
+    assert is_compatible_quantity_increments(Decimal("2"), Decimal("4"))
+
+    assert is_compatible_quantity_increments(Decimal("5"), Decimal("0.1"))
+    assert is_compatible_quantity_increments(Decimal("0.1"), Decimal("5"))
+
+    assert is_compatible_quantity_increments(Decimal("0.99"), Decimal("0.11"))
+    assert is_compatible_quantity_increments(Decimal("0.11"), Decimal("0.99"))
+
+    assert is_compatible_quantity_increments(Decimal("1"), Decimal("0.001"))
+    assert is_compatible_quantity_increments(Decimal("0.001"), Decimal("1"))
+
+    assert not is_compatible_quantity_increments(
+        Decimal("0.99"), Decimal("0.1"))
+    assert not is_compatible_quantity_increments(
+        Decimal("0.1"), Decimal("0.99"))
+
+    assert not is_compatible_quantity_increments(Decimal("5"), Decimal("3"))
+    assert not is_compatible_quantity_increments(Decimal("3"), Decimal("5"))
