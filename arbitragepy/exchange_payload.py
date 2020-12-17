576ea31f3aa8ee35e373ba690a7e59a23c979fb8
astsu <you@example.com>
2020-12-17 19:57:30 +0200

    Added commission field to ExchangePayload dataclass.

--- arbitragepy/exchange_payload.py
+++ arbitragepy/exchange_payload.py
@@ -4,9 +4,18 @@ from decimal import Decimal
 
 @dataclass(frozen=True)
 class ExchangePayload:
-    """This class represents exchange currency data for arbitrage comparison."""
+    """This class represents exchange currency data for arbitrage comparison.
+    
+    Args:
+        price (Decimal): currency price
+        quantity (Decimal): currency quantity
+        quantity_increment (Decimal): quantity increment step
+        min_quantity (Decimal): min currency quantity
+        commission (Decimal): currency ask/bid commission in percent
+    """
 
     price: Decimal
     quantity: Decimal
     quantity_increment: Decimal
     min_quantity: Decimal = Decimal("0")
+    commission: Decimal = Decimal("0")
--- tests/test_exchange_payload.py
+++ tests/test_exchange_payload.py
@@ -12,10 +12,13 @@ def test_exchange_payload() -> None:
     assert p.quantity == Decimal("200.002")
     assert p.quantity_increment == Decimal("0.001")
     assert p.min_quantity == Decimal("0")
+    assert p.commission == Decimal("0")
 
     p = ExchangePayload(
         price=Decimal("100.15"),
         quantity=Decimal("200.002"),
         quantity_increment=Decimal("0.001"),
-        min_quantity=Decimal("1.10"))
+        min_quantity=Decimal("1.10"),
+        commission=Decimal("1"))
     assert p.min_quantity == Decimal("1.10")
+    assert p.commission == Decimal("1")
