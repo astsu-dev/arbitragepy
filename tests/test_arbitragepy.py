85b50d5bac45b043b73ce8e13171dbf93cad780d
astsu <you@example.com>
2020-12-17 20:15:21 +0200

    Added get_spread function.

---
+++ tests/test_arbitragepy.py
@@ -0,0 +1,10 @@
+from decimal import Decimal
+
+from arbitragepy import get_spread
+
+
+def test_get_spread() -> None:
+    assert get_spread(Decimal("100"), Decimal("200")) == Decimal("100")
+    assert get_spread(Decimal("200"), Decimal("100")) == Decimal("-50")
+    assert get_spread(Decimal("0.5"), Decimal("2.0")) == Decimal("300")
+    assert get_spread(Decimal("0.5"), Decimal("0.75")) == Decimal("50")
