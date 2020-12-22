4c3fc213d455df1d3e641bfbeeef441d1d3f2019
astsu <you@example.com>
2020-12-22 13:08:30 +0200

    Added minus_fee function.

--- arbitragepy/__init__.py
+++ arbitragepy/__init__.py
@@ -43,3 +43,17 @@ def plus_fee(n: Decimal, fee: Decimal) -> Decimal:
     """
 
     return n / (1 - fee / 100)
+
+
+def minus_fee(n: Decimal, fee: Decimal) -> Decimal:
+    """Returns the number to which, if you add the `fee` percent, you get `n`
+
+    Args:
+        n (Decimal)
+        fee (Decimal): fee in percent which greater than 0
+
+    Returns:
+        Decimal
+    """
+
+    return n / (1 + fee / 100)
--- tests/test_arbitragepy.py
+++ tests/test_arbitragepy.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 
-from arbitragepy import get_spread, is_compatible_quantity_increments, plus_fee
+from arbitragepy import (get_spread, is_compatible_quantity_increments,
+                         minus_fee, plus_fee)
 
 
 def test_get_spread() -> None:
@@ -33,5 +34,10 @@ def test_is_compatible_quantity_increments() -> None:
 
 
 def test_plus_fee() -> None:
-    assert plus_fee(Decimal(100), Decimal(50)) == Decimal(200)
-    assert plus_fee(Decimal(70), Decimal(30)) == Decimal(100)
+    assert plus_fee(Decimal("100"), Decimal("50")) == Decimal("200")
+    assert plus_fee(Decimal("70"), Decimal("30")) == Decimal("100")
+
+
+def test_minus_fee() -> None:
+    assert minus_fee(Decimal("60"), Decimal("20")) == Decimal("50")
+    assert minus_fee(Decimal("294.4"), Decimal("15")) == Decimal("256")
