9330c59db32f254d88caa014e384c6155e12879f
astsu <you@example.com>
2020-12-31 11:48:21 +0200

    Update test_currency_payload test after rename commission to fee.

--- tests/test_currency_payload.py
+++ tests/test_currency_payload.py
@@ -12,16 +12,16 @@ def test_currency_payload() -> None:
     assert p.quantity == Decimal("200.002")
     assert p.quantity_increment == Decimal("0.001")
     assert p.min_quantity == Decimal("0")
-    assert p.commission == Decimal("0")
-    assert not p.ask_commission_in_current_currency
+    assert p.fee == Decimal("0")
+    assert not p.ask_fee_in_current_currency
 
     p = CurrencyPayload(
         price=Decimal("100.15"),
         quantity=Decimal("200.002"),
         quantity_increment=Decimal("0.001"),
         min_quantity=Decimal("1.10"),
-        commission=Decimal("1"),
-        ask_commission_in_current_currency=True)
+        fee=Decimal("1"),
+        ask_fee_in_current_currency=True)
     assert p.min_quantity == Decimal("1.10")
-    assert p.commission == Decimal("1")
-    assert p.ask_commission_in_current_currency
+    assert p.fee == Decimal("1")
+    assert p.ask_fee_in_current_currency
