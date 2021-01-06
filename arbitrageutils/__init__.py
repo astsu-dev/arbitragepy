2eedb3c3b62e894056bfd9d2fa0b88cfc41294c8
astsu <you@example.com>
2021-01-06 10:49:53 +0200

    Added the create_orders_data import to arbitrageutils/__init__.py.

--- arbitrageutils/__init__.py
+++ arbitrageutils/__init__.py
@@ -1,5 +1,8 @@
 from .currency_payload import CurrencyPayload
 from .fee import minus_fee, plus_fee
+from .order_data import (OrderData, OrderPayload, OrdersData,
+                         create_ask_order_data, create_bid_order_data,
+                         create_orders_data)
 from .quantity_increment import (is_compatible_quantity_increments,
                                  to_compatible_quantity_increment)
 from .spread import get_spread
